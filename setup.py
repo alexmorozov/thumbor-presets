d956d0956402366272668b65cf1ab1f15b603de2
Alex Morozov <inductor2000@mail.ru>
2016-02-15 11:53:24 +0300

    Fixed the version mismatch causing bumpversion to ignore setup.py.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="thumbor_presets",
-    version='0.1.0',
+    version='0.1.2',
     url='http://github.com/alexmorozov/thumbor-presets',
     author='Alex Morozov',
     author_email='inductor2000@mail.ru',
