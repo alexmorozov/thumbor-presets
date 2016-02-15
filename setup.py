63739a52f900bb8505c9703311b54f3bacd68056
Alex Morozov <inductor2000@mail.ru>
2016-02-15 12:17:12 +0300

    Aha, that's why setup.py wasn't being updated.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="thumbor_presets",
-    version='0.1.2',
+    version='0.1.3',
     url='http://github.com/alexmorozov/thumbor-presets',
     author='Alex Morozov',
     author_email='inductor2000@mail.ru',
